d11798d6a731e01843b229cff2a862bdd6100b37
ashi1901-as <sinha.ashi2004@gmail.com>
2024-06-16 12:32:15 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const account_id = 144553
+let account_email = "ashi.sinha@gmail.com"
+var account_pwd = "12345"
+account_city="patna"
+let account_state;
+
+//account_id=2//not allowed
+console.log(account_id);
+console.table([account_email,account_id,
+    account_pwd,account_state,account_city]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+//"use strict"; // treat all jscodes as newer version
+//alert(3+30); using nodejs not browser
+let name="ashi"
+let age=10
+let isloggedin=false
+//number
+//bigint
+//string
+//boolean
+//null=standalone value
+//undefined 
+//symbol=use to identifies the uniquness of value
+//object
+
+console.log(typeof "hitesth");
+console.log(typeof null);
+console.log(typeof undefined);
