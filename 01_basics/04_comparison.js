3435bb57fe62944dfd7e5114bc10b1feffeaf260
ashi1901-as <sinha.ashi2004@gmail.com>
2024-06-17 18:18:59 +0000

    conversions

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,10 @@
+/*console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+//in this the comparison operator >,<,>=,<= 
+//converts the null to number 0 and treat differently while the equality check treat differently.
+
+=== strict check
+same datatype conversion is easy
+*/
+console.log("2"===2)
