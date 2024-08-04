1416122f1f3a3ec071aeb3ba7649b3168036c284
ashi1901-as <sinha.ashi2004@gmail.com>
2024-08-04 18:35:04 +0000

    js

---
+++ 03_basics/05_file.js
@@ -0,0 +1,14 @@
+//javascript execution context
+//global execution context use this as keyword
+//functional execution context
+//eval execution context
+//memory creation phase
+//execution phase=code
+let val1=10;
+let val2=20;
+function addnum(num1,num2)
+{
+    let t=num1+num2;
+    return t;
+}
+let r1=addnum(val1,val2)
